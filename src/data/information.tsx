c56a863fc0fe0948a03298dd894a1ef3591dae88
Patricia <patriciaejiang@gmail.com>
2024-01-09 09:59:29 -0800

    initial commit

---
+++ src/data/information.tsx
@@ -0,0 +1,17 @@
+export const initialProducts: IProduct[] = [
+    {
+        name: 'Car 1',
+        images: '/images/car.jpg',
+        price: 20000
+    },
+    {
+        name: 'Car 2',
+        images: '/images/car2.jpg',
+        price: 30000
+    },
+    {
+        name: 'Car 3',
+        images: '/images/car3.png',
+        price: 10000
+    }
+]
---
+++ typings.d.ts
@@ -0,0 +1,5 @@
+interface IProduct{
+    name: string
+    images: string
+    price: number
+}
